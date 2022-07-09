dab8816a8daba131af94d78924afb63ee57d0d1a
Andy Peralta <aperalta686@gmail.com>
2022-07-08 23:50:49 -0400

    Improved Website Look 	- HomePage 	- Editing a Campground 	- Adding a Campground

--- YelpCamp/public/javascripts/map.js
+++ YelpCamp/public/javascripts/map.js
@@ -1,11 +1,12 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 container: 'map', // container ID
-style: 'mapbox://styles/mapbox/streets-v11', // style URL
+style: 'mapbox://styles/mapbox/light-v10', // style URL
 center: campground.geometry.coordinates, // starting position [lng, lat]
 zoom: 4 // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl());
 new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
   .setPopup(
