c172c7f51bb2a39927180b936785f2315323348f
Andy Peralta <aperalta686@gmail.com>
2022-07-10 13:54:49 -0400

    remove unnecessary code

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -13,14 +13,14 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local')
 const User = require('./models/user') 
-
+const helmet = require('helmet')
 const mongoSanitize = require('express-mongo-sanitize');
-
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-
-mongoose.connect('mongodb://localhost:27017/yelpcamp');
+const MongoStore = require("connect-mongo");
+const dbUrl = process.env.DB_URL ||'mongodb://localhost:27017/yelpcamp';
+mongoose.connect(dbUrl);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
@@ -38,10 +38,23 @@ app.use(mongoSanitize({
     replaceWith: '_'
 }
 ));
+const secretKy = process.env.SECRET || 'thisissupersecret';
+const store =  MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: secretKy
+    }
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
 
 const sessionConfig = {
+    store,
     name: "session",
-    secret: 'thisissupersecret',
+    secret: secretKy,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -53,6 +66,52 @@ const sessionConfig = {
 };
 app.use(session(sessionConfig));
 app.use(flash());
+const scriptUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+];
+const connectUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontUrls = [];
+app.use(helmet({
+    contentSecurityPolicy: ({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob",
+               " data:",
+               "https://res.cloudinary.com/dwcswvhik/",
+               "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontUrls]
+        }
+    }),
+    crossOriginEmbedderPolicy: false
+}));
+
 
 app.use(passport.initialize())
 app.use(passport.session())
@@ -93,6 +152,7 @@ app.use((err, req, resp, next) => {
     resp.status(status).render('error', { err });
 })
 
-app.listen(3000, () => {
-    console.log("LISTENING ON PORT 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`LISTENING ON PORT ${port}`);
 })
